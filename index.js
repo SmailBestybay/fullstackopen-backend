d486762ee18d36141fe96f0d8793bb648561b2ae
Smile <smailbestybay@gmail.com>
2023-03-24 18:33:42 -0700

    exercises 3.15-3.18 works locally

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ const app = express()
 const Person = require('./models/person')
 const cors = require('cors')
 
+morgan.token('req-body', (request) =>  JSON.stringify(request.body))
+
 app.use(express.static('build'))
 app.use(express.json())
-app.use(cors())
-
-morgan.token('req-body', (request) =>  JSON.stringify(request.body))
 app.use(morgan(
   ':method :url request body: :req-body',
   {
@@ -18,29 +17,7 @@ app.use(morgan(
   }
 ))
 app.use(morgan('tiny'))
-
-let persons = [
-  {
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
+app.use(cors())
 
 app.get('/', (request, response) => {
   response.send('<h1>Welcome to Phonebook!</h1>')
@@ -52,12 +29,6 @@ app.get('/api/persons', (request, response) => {
       response.json(persons)
     })
 })
-
-const generateId = () => {
-  const min = persons.length
-  const max = persons.length + 1000;
-  return Math.floor((Math.random() * (max - min) + min))
-}
 
 app.post('/api/persons', (request, response) => {
 
@@ -82,30 +53,65 @@ app.post('/api/persons', (request, response) => {
 
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person
     .findById(request.params.id)
     .then(person => {
-      response.json(person)
+      if(person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
     })
-    .catch(error => {
-      response.status(404).end()
-    })
+    .catch(error => next(error))
 })
 
 app.get('/api/info', (request, response) => {
-  const entries = `Phonebook has info for ${persons.length} people`
-  const recievedOn = new Date()
-  response.send(`${entries} <br> ${recievedOn}`)
-})
-
-app.delete('/api/persons/:id', (request, response) => {
   Person
-    .findByIdAndDelete(request.params.id)
-    .then(success => {
-      response.status(204).end()
+    .find({})
+    .then(persons => {
+      const entries = `Phonebook has info for ${persons.length} people`
+      const recievedOn = new Date()
+      response.send(`${entries} <br> ${recievedOn}`)
     })
 })
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person
+    .findByIdAndRemove(request.params.id)
+    .then(success => response.status(204).end())
+    .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const person = {
+    name: request.body.name,
+    number: request.body.number,
+  }
+
+  Person
+    .findByIdAndUpdate(request.params.id, person, {new: true})
+    .then(updatedPerson => response.json(updatedPerson))
+    .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT , () => {
