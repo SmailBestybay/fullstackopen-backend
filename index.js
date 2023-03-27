026c3bd0f8951686ddd820e41aec0249baaec8dc
Smile <smailbestybay@gmail.com>
2023-03-27 14:57:09 -0700

    complete exercises 3.19 and 3.20

--- index.js
+++ index.js
@@ -19,10 +19,6 @@ app.use(morgan(
 app.use(morgan('tiny'))
 app.use(cors())
 
-app.get('/', (request, response) => {
-  response.send('<h1>Welcome to Phonebook!</h1>')
-})
-
 app.get('/api/persons', (request, response) => {
   Person.find({})
     .then(persons => {
@@ -30,7 +26,7 @@ app.get('/api/persons', (request, response) => {
     })
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
 
   if(!request.body.name) {
     return response.status(400).json({error: 'name missing'})
@@ -50,6 +46,7 @@ app.post('/api/persons', (request, response) => {
     .then(savedPerson => {
       response.json(savedPerson)
     })
+    .catch(error => next(error))
 
 })
 
@@ -90,7 +87,11 @@ app.put('/api/persons/:id', (request, response, next) => {
   }
 
   Person
-    .findByIdAndUpdate(request.params.id, person, {new: true})
+    .findByIdAndUpdate(
+      request.params.id, 
+      person, 
+      {new: true, runValidators: true, context: 'query'},
+      )
     .then(updatedPerson => response.json(updatedPerson))
     .catch(error => next(error))
 })
@@ -100,7 +101,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
   next(error)
 }
